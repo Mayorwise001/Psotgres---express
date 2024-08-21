10351f555eb830c0c07d7d37e566e47cc8c2f02e
mayorwise001 <tomosorijoseph@yahoo.com>
2024-08-21 15:09:50 +0100

    added route

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const indexRouter = require('./routes/index')
+const path = require('path');
+const logger = require('morgan');
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
-
+app.set('views', path.join(__dirname, 'views'));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.get('/', (req, res) => {
 //   res.send('index');
